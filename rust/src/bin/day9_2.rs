7a7068b3f902d4729381a71d506ee026ba273984
Vo Hoai An <vohoaian.tn@gmail.com>
2023-12-10 00:01:00 +0700

    day 9 rust

---
+++ rust/src/bin/day9_2.rs
@@ -0,0 +1,47 @@
+use std::fs;
+use std::path::{Path};
+
+
+fn get_input(p: &str) -> String {
+    let file_path = Path::new(".").join("inputs").join(p);
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    contents
+}
+
+fn cal_diff_array(nums: Vec<i64>) -> Vec<i64> {
+    let mut result: Vec<i64> = vec![];
+
+    for i in 1..nums.len() {
+        result.push(nums[i] - nums[i - 1]);
+    }
+
+    result
+}
+
+fn is_valid_array(nums: &Vec<i64>) -> bool {
+    nums.iter().all(|x| *x == 0)
+}
+
+fn main() {
+    let inp = get_input("day9_1_2.inp");
+    let lines: Vec<String> = inp.lines().map(|x| x.to_string()).collect();
+
+    // println!("{:?}", lines);
+    let mut sum: i64 = 0;
+
+    for line in lines {
+        let mut nums: Vec<i64> = line.split(" ").map(|x| x.parse().expect("Should be convert to i64")).collect();
+        let mut last_nums: Vec<i64> = vec![];
+        last_nums.insert(0, *(nums.first().expect("Could not be empty")));
+
+        while !is_valid_array(&nums) {
+            nums = cal_diff_array(nums);
+            last_nums.insert(0, *(nums.first().expect("Could not be empty")));
+        }
+        let s: i64 = last_nums.into_iter().fold(0, |a, b| b - a);
+        sum += s;
+    }
+
+    println!("{}", sum);
+}
